ac5d33e599ffe6e29c559c50c57e1b5eca0bf974
unknown <lukoninaev@04211-pc.i-free.local>
2016-08-16 15:46:24 +0300

    add homework for lesson-1

---
+++ lesson-1/lukoninaev/index.ts
@@ -0,0 +1,54 @@
+import RecursiveMenu from './menu/menu';
+
+// ??? нужно ли здесь при задании переменной указывать тип
+// ??? или достаточно, того, что этот тип указан в аргументе функции, куда передается переменная
+// type recursiveMenuDataT = {title: string, items?: recursiveMenuDataT}[];
+// let menuData:recursiveMenuDataT = ...;
+
+let menuData = [
+    {
+        title: 'Животные',
+        items: [
+            {
+                title: 'Млекопитающие',
+                items: [
+                    {title: 'Коровы'},
+                    {title: 'Ослы'},
+                    {title: 'Собаки'},
+                    {title: 'Тигры'}
+                ]
+            }, {
+                title: 'Другие',
+                items: [
+                    {title: 'Змеи'},
+                    {title: 'Птицы'},
+                    {title: 'Ящерицы'},
+                ],
+            }
+        ]
+    },
+    {
+        title: 'Рыбы',
+        items: [
+            {
+                title: 'Аквариумные',
+                items: [
+                    {title: 'Гуппи'},
+                    {title: 'Скалярии'}
+                ]
+            },
+            {
+                title: 'Форель',
+                items: [
+                    {title: 'Морская форель'}
+                ]
+            }
+        ]
+    }
+];
+
+// ??? нужно ли здесь при задании переменной указывать тип document.getElementById('menu') as HTMLElement
+// ??? или достаточно, того, что этот тип указан в конструкторе класса, куда передается переменная
+let menuNode = document.getElementById('menu');
+
+new RecursiveMenu(menuData, menuNode);
