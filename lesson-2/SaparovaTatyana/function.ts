a5e028e22f6d4b2984fa7b67e9a55fb2d8a5b230
Tatyana <an-tatys@mail.ru>
2016-08-18 12:52:39 +0300

    task-2

--- lesson-2/SaparovaTatyana/function.ts
+++ lesson-2/SaparovaTatyana/function.ts
@@ -2,10 +2,8 @@
 //    Возвращает true, если все аргументы, кроме первого входят в первый.
 //    Первым всегда должен быть массив.
 
-// TODO: предусмотреть дубли
 function isInArray (numberArray: (number|string|boolean)[], ...numbers: (number|string|boolean)[]):boolean {
     let result:any[] = [];
-
 
     if (numberArray.length !== numbers.length) return false;
 
@@ -22,4 +20,38 @@ function isInArray (numberArray: (number|string|boolean)[], ...numbers: (number|
 console.log( isInArray([2, 8, "foo", false], false, 'gav', 3, 6) );
 console.log( isInArray([2, 8, "foo", false], false, 'foo', 2, 8, 8, true ) );
 console.log( isInArray([2, 8, "foo", false], false, 'foo', 8, false ) );
-console.log( isInArray([2, 8, "foo", false], false, 'foo', 8, 2 ) );
+console.log( isInArray([2, 2, "foo", false], false, 'foo', 8, 2 ) );
+console.log( isInArray([2, 8, "foo", false], false, 'foo', 2, 8 ) );
+
+// isInArray(a, ...b){
+//  if(!b || b.length !== a.length) return false;
+ 
+// for(let i= 0; i<a.lenght; i++)
+//    if (a.indexOf(b[i]) === -1) return false;
+// }
+
+// return true;
+// }
+
+
+
+
+// 2) писать функцию summator(), которая сумирует переданые ей аргументы.
+//    Аргументы могут быть либо строкового либо числового типа. Количество их не ограничено
+
+function summator (...elem: number[]):number;
+function summator (...elem: string[]):string;
+function summator (...elem: (string|number)[]):string {
+
+    let sum = 0;
+    for (let el of elem){
+       sum = +sum + el;
+    }
+
+    return `Summa = ${sum}`;
+}
+
+console.log(summator(2, 4, 4, 5, 2));
+console.log(summator('2', '4'));
+console.log(summator('lesson', '4'));
+
