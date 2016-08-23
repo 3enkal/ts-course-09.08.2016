0a997eb575526c5d58fc1f25accaf892391e09f3
arseny <arsenykonohov@gmail.com>
2016-08-23 17:35:30 +0300

    arsenykonohov | lesson #2 | reverseText - done

--- lesson-2/arseny_konohov/script/3_getunique/3_getunique.ts
+++ lesson-2/arseny_konohov/script/3_getunique/3_getunique.ts
@@ -16,4 +16,4 @@ console.log("/ getUnic / getUnic / getUnic / getUnic / getUnic /");
 
 console.log(getUnic <(number|string|boolean)> (1, "3", 12, 1, false, true, 1, 4, 3, "2", "lsdf", false, "3", 12, 1, false, true, 1, 1, false, true, 1));
 
-console.log("\n")
+console.log("\n");
